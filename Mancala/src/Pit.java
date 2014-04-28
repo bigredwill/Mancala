3acaf97c27670d01166dd9aeebfdde5e79f93789
avidey94 <avidey94@gmail.com>
2014-04-27 17:05:18 -0700

    5:05 pm
    wtf

--- Mancala/src/Pit.java
+++ Mancala/src/Pit.java
@@ -4,77 +4,92 @@ import java.awt.geom.Ellipse2D;
 
 /**
  * An individual pit.
+ *
  * @author Avi, Will, Michael
  */
-public class Pit {
-    
+public class Pit
+{
+
     private int player;
     private int marbles;
     private boolean isEnd;
+
     /**
      * Constructs a pit with specified number of marbles.
-     * @param someMarbles 
+     *
+     * @param someMarbles
      */
     public Pit(int someMarbles, int aPlayer, boolean isEnd)
     {
         this.isEnd = isEnd;
         this.player = aPlayer;
         this.marbles = someMarbles;
+        
     }
-    
+
+
     /**
      * Gets if end pit
-     * @return 
+     *
+     * @return
      */
-    public boolean isIsEnd() {
+    public boolean isIsEnd()
+    {
         return isEnd;
     }
 
     /**
      * Sets if end pit
-     * @param isEnd 
+     *
+     * @param isEnd
      */
-    public void setIsEnd(boolean isEnd) {
+    public void setIsEnd(boolean isEnd)
+    {
         this.isEnd = isEnd;
     }
-    
+
     /**
      * Gets the player number (1 or 2)
-     * @return 
+     *
+     * @return
      */
     public int getPlayer()
     {
         return this.player;
     }
-    
+
     public void setPlayer(int aPlayer)
     {
         this.player = aPlayer;
     }
+
     /**
      * Get the number of models in the pit.
-     * @return 
+     *
+     * @return
      */
     public int getMarbles()
     {
         return this.marbles;
     }
-    
+
     /**
      * Set the number of marbles in the pit.
-     * @param someMarbles 
+     *
+     * @param someMarbles
      */
     public void setMarbles(int someMarbles)
     {
         this.marbles = someMarbles;
     }
-    
+
     /**
      * Adds one marble to the pit.
      */
     public void addMarble()
     {
         this.marbles++;
+        System.out.println("adding marble " + getMarbles());
     }
 
     @Override
