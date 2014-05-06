2f1a833c17be7767a5d290b3f345d51683d0d923
avidey94 <avidey94@gmail.com>
2014-05-06 13:31:27 -0700

    Added TrippyTheme
    trilly

--- Mancala/src/HomeFrame.java
+++ Mancala/src/HomeFrame.java
@@ -1,79 +1,86 @@
 
 import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author michaeldaniels
  */
-public class HomeFrame extends JFrame
-{
+public class HomeFrame extends JFrame {
+
     private Model model;
-    
-    public HomeFrame()
-    {
+
+    public HomeFrame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         final JPanel mainFrame = new JPanel();
+        try {
+            BufferedImage in = ImageIO.read(this.getClass().getResource("/Images/endPit1.png"));
+            
+            //this.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File(this.getClass().getResource("/Images/endPit1.png"))))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //JLabel label = new JLabel(new ImageIcon("/Images/tripEnd.gif"));
+        //mainFrame.add(label);
+
+        //mainFrame.drawImage(background, 0, 0, null);
+        //mainFrame.setBackground(Color.green);
         JButton whiteTheme = new JButton("White Theme");
         JButton blackTheme = new JButton("Black Theme");
         JButton spongebobTheme = new JButton("Spongebob Theme");
+        JButton trippyTheme = new JButton("TRIPPPPYYYYyyYYYYyY");
         model = new Model(4, Color.WHITE);
-        
+
         final JTextField numMarbles = new JTextField("4");
-        
+
         mainFrame.add(whiteTheme);
         mainFrame.add(blackTheme);
         mainFrame.add(spongebobTheme);
         mainFrame.add(numMarbles);
+        mainFrame.add(trippyTheme);
         setContentPane(mainFrame);
         setVisible(true);
         pack();
-        
-        whiteTheme.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
+
+        whiteTheme.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 int marbles = Integer.parseInt(numMarbles.getText());
-                if (marbles > 4 || marbles < 1)
-                {
+                if (marbles > 4 || marbles < 1) {
                     JOptionPane.showMessageDialog(mainFrame, "Enter a number between 1 and 4");
-                }
-                else
-                {
-                   // GameFrame game = new GameFrame(new Model(marbles), Color.WHITE);
+                } else {
+                    // GameFrame game = new GameFrame(new Model(marbles), Color.WHITE);
                     model = new Model(marbles, Color.WHITE);
-                    
+
                     setVisible(false);
                     GameFrame gf = new GameFrame(model, new WhiteTheme());
                     gf.setVisible(true);
                 }
             }
         });
-        
-        blackTheme.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
+
+        blackTheme.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 int marbles = Integer.parseInt(numMarbles.getText());
-                if (marbles > 4 || marbles < 1)
-                {
+                if (marbles > 4 || marbles < 1) {
                     JOptionPane.showMessageDialog(mainFrame, "Enter a number between 1 and 4");
-                }
-                else
-                {   
+                } else {
                     ///GameFrame game = new GameFrame(new Model(marbles), Color.BLACK);
                     model = new Model(marbles, Color.BLACK);
                     setVisible(false);
@@ -82,18 +89,14 @@ public class HomeFrame extends JFrame
                 }
             }
         });
-        
-        spongebobTheme.addActionListener(new ActionListener() {
 
+        spongebobTheme.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                                int marbles = Integer.parseInt(numMarbles.getText());
-                if (marbles > 4 || marbles < 1)
-                {
+                int marbles = Integer.parseInt(numMarbles.getText());
+                if (marbles > 4 || marbles < 1) {
                     JOptionPane.showMessageDialog(mainFrame, "Enter a number between 1 and 4");
-                }
-                else
-                {
+                } else {
                     ///GameFrame game = new GameFrame(new Model(marbles), Color.BLACK);
                     model = new Model(marbles, Color.BLACK);
                     setVisible(false);
@@ -102,10 +105,25 @@ public class HomeFrame extends JFrame
                 }
             }
         });
+
+        trippyTheme.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int marbles = Integer.parseInt(numMarbles.getText());
+                if (marbles > 4 || marbles < 1) {
+                    JOptionPane.showMessageDialog(mainFrame, "Enter a number between 1 and 4");
+                } else {
+                    ///GameFrame game = new GameFrame(new Model(marbles), Color.BLACK);
+                    model = new Model(marbles, Color.BLACK);
+                    setVisible(false);
+                    GameFrame gf = new GameFrame(model, new TrippyTheme());
+                    gf.setVisible(true);
+                }
+            }
+        });
     }
-    
-    public Model getModel()
-    {
+
+    public Model getModel() {
         return this.model;
     }
 }
---
+++ Mancala/src/TrippyTheme.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Avi
+ */
+public class TrippyTheme implements BoardTheme{
+
+    @Override
+    public String getMarbleFile() {
+        String mb = "/Images/marb2.png";
+        return mb;
+    }
+
+    @Override
+    public String getEndPitFile() {
+//        return "/Images/endPit2.png";
+        return "/Images/tripEnd.gif";
+    }
+
+    @Override
+    public String getPitFile() {
+//        return "/Images/pit2.png";
+        return "/Images/tripPit.gif";
+    }
+    
+    
+}
